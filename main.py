b78a9af1c4247170c41f8df7115480e812bc0012
nicolaskawer <nicolaskawer@gmail.com>
2022-05-27 12:56:28 +0300

    some_problem

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+#def linear_interpolation(dict1):
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
@@ -21,6 +23,10 @@ if __name__ == '__main__':
         helper = {x: y}
         dictionary.update(helper)
     print(dictionary)
+    x_f = float(input("Enter the point u want to calculate: "))
+    print(dictionary[0])
+    """יש פה בעייה להמשך כי נגיד בלינארית אני צריך שתי נקודות שהנקודה נמצאת בנייהם ואני צריך לבדוק נקודה נקודה כלומר לעבור על המילון שלי ואני לא יכול לעבור כמו שאת רואה 
+    למעלה וזה בעיה
 
 
 
