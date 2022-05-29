5952035fc80eebfcae5435503398d188a8c61b3e
nicolaskawer <nicolaskawer@gmail.com>
2022-05-30 00:25:51 +0300

    some_problem_with

--- main.py
+++ main.py
@@ -7,7 +7,7 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+#
 def linear_interpolation(arr, x_f1):
     for i in range(len(arr[0])):
         if arr[0][i] > x_f1:
