3faae1c6c0a41a76e0f7b8a51e791a59700e6ba5
nicolaskawer <nicolaskawer@gmail.com>
2022-05-29 18:12:14 +0300

    some_problem2

--- main.py
+++ main.py
@@ -100,9 +100,9 @@ def multiply_two_matrix(matrix1, matrix2):
 def Polynomial_interpolation(arr, x_f1):
     strong_matrix = []
     answer_matrix = arr[1]
-    for i in range(len(arr[0])-1):
+    for i in range(len(arr[0])):
         helper = []
-        for j in range(len(arr[0])-1):
+        for j in range(len(arr[0])):
             helper.append(arr[0][i] ** j)
         strong_matrix.append(helper)
     #print(strong_matrix)
@@ -133,23 +133,63 @@ def lagrange_interpolation(arr, x_f1):
         Pn += Li*arr[1][i]
     return Pn
 
+def Spline_Kobe(arr, x_f1):
+    hi = []
+    gama = []
+    mi = []
+    di = []
+    new_matrix = []
+    for i in range(len(arr[0])-1):
+        hi.append(arr[0][i+1] - arr[0][i])
+    for i in range(len(hi)-1):
+        gama.append(hi[i]/(hi[i]+hi[i+1]))
+        mi.append(1-gama[i])
+    for i in range(1,len(arr[0])-1):
+        di.append((6/(hi[i-1]+hi[i]))*(((arr[1][i+1]-arr[1][i])/hi[i])-((arr[1][i]-arr[1][i-1])/hi[i-1])))
+    for i in range(size):
+        helper = []
+        for j in range(size):
+            if i == j:
+                helper.append(2)
+            elif j == i+1:
+                helper.append(gama[i+1])
+            elif j == i-1:
+                helper.append(mi[i+1])
+        new_matrix.append(helper)
+    multiply_elementary_matrix = gauss_method(new_matrix)
+    for i in range(size):
+        final_result.append(0)
+    for r in range(len(multiply_elementary_matrix)):
+        for c in range(len(multiply_elementary_matrix)):
+            final_result[r] += multiply_elementary_matrix[r][c] * di[c]
+
+    for i in range(len(arr[0])):
+        if arr[0][i] > x_f1:
+            x_1 = arr[0][i - 1]
+            x_2 = arr[0][i]
+            s_x = (((x_2-x_f1)**3)*mi[i-1]+((x_f1-x_1)**3)*mi[i])/(6*hi[i-1])+(((x_2-x_f1)*arr[1][i-1]+(x_f1-x_1)*arr[1][i])/hi[i-1]) -(((x_2-x_f1)*mi[i-1]+(x_f1-x_1)*mi[i])*hi[i-1])/6
+            break
+
+    return s_x
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     "לא לשכוח להוסיף הערות לבודק על הטבלת ערכים הזו מתחת"
-    two_dimensional_array = tuple([[1, 2, 3, 4], [0.8415, 0.9093, 0.1411, 8]])
+    two_dimensional_array = tuple([[1, 2, 3, 4], [0, 0.5, 0.7072, 1]])
     x_f = 2.5
     #another example:
     #two_dimensional_array = tuple([[1, 2, 4], [1, 0, 1.5]])
     #x_f = 3
     final_result = []
     answer_matrix = []
+    size = len(two_dimensional_array[0])
     result = linear_interpolation(two_dimensional_array, x_f)
     print(result)
-    size = len(two_dimensional_array[0]) - 1
     result = Polynomial_interpolation(two_dimensional_array, x_f)
     print(result)
     print(lagrange_interpolation(two_dimensional_array, x_f))
+    size = len(two_dimensional_array[0]) - 2
+    print(Spline_Kobe(two_dimensional_array, x_f))
     """helper = {}
     size = int(input("enter how many point do u have: "))
     for i in range(size):
